dce4a0e4201ee724fcccb9f6bc57ae3743e98394
Anton Baksheiev <abaksheiev@gmail.com>
2015-12-11 15:28:54 +0000

    add jasmin to project

--- testsClient/phones/phoneListCtrlTest.js
+++ testsClient/phones/phoneListCtrlTest.js
@@ -1,18 +1,7 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
-describe('PhoneCat controllers', function() {
-
-  describe('PhoneListCtrl', function(){
-
-    beforeEach(module('phonecatApp'));
-
-    it('should create "phones" model with 3 phones', inject(function($controller) {
-      var scope = {},
-          ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
-      expect(scope.phones.length).toBe(3);
-    }));
-
+describe("A suite", function() {
+  it("the first test", function() {
+    expect(true).toBe(true);
   });
 });
