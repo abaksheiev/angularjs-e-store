43bf4793dbd71abbc355fb5474b05ab680f973e3
Anton Baksheiev <abaksheiev@gmail.com>
2015-12-11 22:21:50 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'angularjs-e-store',
+    basePath: '/',
 
 
     // frameworks to use
