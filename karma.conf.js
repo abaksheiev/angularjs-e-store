8aee2d4a202efbfa9dc65575236022e707fc2f61
Anton Baksheiev <abaksheiev@gmail.com>
2015-12-12 01:03:53 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'app/*.js', 
       'app/components/phones/*.js', 
       'testsClient/*/*.js'
     ],
